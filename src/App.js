e9990ac18e5f6cc1e7da29f7a858e7ae8b747cbc
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-11-07 14:25:27 -0500

    agregando router app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './view/Home';
+import Esika from './view/Esika';
+import LBel from './view LBel';
+import Cyzone from './view/Cyzone';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route
+        path='/'
+        exact
+        component={Home}
+      />
+      <Route
+        path='/esika'
+        exact
+        component={Esika}
+      />
+      <Route
+        path='/lbel'
+        exact
+        component={LBel}
+      />
+      <Route
+        path='/cyzone'
+        exact
+        component={Cyzone}
+      />
+    </Switch>
+  </Router>
+);
+
 
 export default App;
