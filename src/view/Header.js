01a62292430b8c01f2ce40638c28d465c4dde4bc
Nataly Porras <natalypc27@gmail.com>
2018-11-08 15:41:57 -0500

    estilos

--- src/view/Header.js
+++ src/view/Header.js
@@ -8,10 +8,10 @@ const Header = ({ itemsCount }) => {
     <nav className="navbar contentNav">
       <Link to="/" className="navbar-brand"><img src={Logo} className="logo" alt="Logo de Belcorp" /></Link>
       <div className="nav">
-        <Link to="/esika" className="m-2">Esika</Link>
-        <Link to="/lbel" className="m-2">L'bel</Link>
-        <Link to="/cyzone" className="m-2">Cyzone</Link>
-        <Link to="/shopList" className="m-2"><i className="fas fa-shopping-bag text-white"></i><span className="badge badge-pill badge-danger">{itemsCount}</span></Link>
+        <Link to="/Belcorp-Hackaton-2018/esika" className="m-2">Esika</Link>
+        <Link to="/Belcorp-Hackaton-2018/lbel" className="m-2">L'bel</Link>
+        <Link to="/Belcorp-Hackaton-2018/cyzone" className="m-2">Cyzone</Link>
+        <Link to="/Belcorp-Hackaton-2018/shopList" className="m-2"><i className="fas fa-shopping-bag text-white"></i><span className="badge badge-pill badge-danger">{itemsCount}</span></Link>
       </div>
     </nav>
   )
