83e358da877f0cbf940c6463581e935bd6a839c3
Nataly Porras <natalypc27@gmail.com>
2018-11-08 14:50:13 -0500

    Updates

---
+++ precache-manifest.7b9621131bf50d829411947d038c8a38.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7f39bde8709669fc9e9577665f41b7a",
+    "url": "/Belcorp-Hackaton-2018/static/media/lbelLogo.a7f39bde.PNG"
+  },
+  {
+    "revision": "795396cc236505ac62721e4db6491931",
+    "url": "/Belcorp-Hackaton-2018/static/media/esikaLogo.795396cc.PNG"
+  },
+  {
+    "revision": "6421205e809312afae93aede8a5ac72d",
+    "url": "/Belcorp-Hackaton-2018/static/media/cyzoneLogo.6421205e.PNG"
+  },
+  {
+    "revision": "1b19ea765c186a56ffceb76d52428d81",
+    "url": "/Belcorp-Hackaton-2018/static/media/belcorp.1b19ea76.png"
+  },
+  {
+    "revision": "b4d30698d38dfd6872e22eb35bf45252",
+    "url": "/Belcorp-Hackaton-2018/static/media/Captura.b4d30698.JPG"
+  },
+  {
+    "revision": "081d556f831a83c7cd3e",
+    "url": "/Belcorp-Hackaton-2018/static/js/runtime~main.081d556f.js"
+  },
+  {
+    "revision": "db99acd016812e607853",
+    "url": "/Belcorp-Hackaton-2018/static/js/main.db99acd0.chunk.js"
+  },
+  {
+    "revision": "be619009e1f21c9e41d2",
+    "url": "/Belcorp-Hackaton-2018/static/js/1.be619009.chunk.js"
+  },
+  {
+    "revision": "db99acd016812e607853",
+    "url": "/Belcorp-Hackaton-2018/static/css/main.b908e508.chunk.css"
+  },
+  {
+    "revision": "37bad76c047b031364a0d01f66580818",
+    "url": "/Belcorp-Hackaton-2018/index.html"
+  }
+];
