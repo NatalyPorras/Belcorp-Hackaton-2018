56f6839e9a754bdbc82ef27a3b3b582aa2c99828
spacedgatito <miluska.gonzalezar@gmail.com>
2018-11-08 15:43:21 -0500

    Cambia form

--- src/view/ContactConsultant.js
+++ src/view/ContactConsultant.js
@@ -1,54 +1,63 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Header from './Header'
 
-const ContactConsultant = ({ itemsCount }) => (
-  <Fragment>
-    <Header itemsCount={itemsCount} />
-    <div className="container mt-4">
-      <div className="row">
-        <form className="col-md-6 offset-md-3">
-          <div className="form-group">
-            <label for="exampleFormControlInput1">Escribe tu correo:</label>
-            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="hola@mail.com" />
-          </div>
-          <p>Encuentra tu consultora por:</p>
-          <div className="form-group">
-            <div className="checkbox">
-              <label data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                <input type="checkbox" />
-                código</label>
-            </div>
-          </div>
-          <div id="collapseOne" aria-expanded="false" className="collapse">
-            <div className="form-group">
-              <input type="number" className="form-control" id="exampleFormControlInput1" />
-            </div>
-            <div class="form-group">
-              <label for="formGroupExampleInput">Example label</label>
-              <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
-            </div>
-            <div class="form-group">
-              <label for="formGroupExampleInput2">Another label</label>
-              <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input" />
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="checkbox">
-              <label data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                <input type="checkbox" />zona</label>
-            </div>
-          </div>
-          <div id="collapseTwo" aria-expanded="true" className="collapse in">
-            <div>
+class ContactConsultant extends Component {
 
-            </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      consultantCode: null,
+      consultants: [
+        { code: 123456, name: 'Ailim Moscoso', phoneNumber: 980671333, email: 'ailim.moscoso@gmail.com' },
+        { code: 654321, name: 'Miluska González', phoneNumber: 933500732, email: 'miluska.gonzalezar@gmail.com' },
+        { code: 554433, name: 'Mely Hidalgo', phoneNumber: 929940441, email: 'mely.hidalgo.crespo@gmail.com' },
+        { code: 334455, name: 'Nataly Porras', phoneNumber: 953211790, email: 'natalypc27@gmail.com' }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Header itemsCount={this.props.itemsCount} />
+        <div className="container mt-4">
+          <div className="row">
+            <form className="col-md-6 offset-md-3">
+              <div className="form-group">
+                <label for="exampleFormControlInput1">Escribe tu correo:</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1" />
+              </div>
+              <div className="form-group">
+                <label for="exampleFormControlInput1">Teléfono*</label>
+                <input type="number" className="form-control" id="exampleFormControlInput1" />
+              </div>
+              <p><b>Si ya tienes una consultora:</b></p>
+              <div className="form-group">
+                <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="Ingresa su código aquí" />
+              </div>
+              <div className="form-group">
+
+                <div className="checkbox">
+                  
+                  <label data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                  <b>O si aún no tienes una, búscala por:</b>
+                  </label>
+                  <p><input type="checkbox" /> Ubicación</p>
+                </div>
+              </div>
+              <div id="collapseTwo" aria-expanded="true" className="collapse in">
+                <div>
+
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
 
-    </div>
+        </div>
 
-  </Fragment>
-);
+      </Fragment>
+    )
+  }
+};
 
 export default ContactConsultant;
