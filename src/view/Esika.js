28c54234b5bb46c84e145c3c34f452a645357638
Nataly Porras <natalypc27@gmail.com>
2018-11-08 08:01:48 -0500

    actualizando cambios

--- src/view/Esika.js
+++ src/view/Esika.js
@@ -1,4 +1,4 @@
-import React,{Component , Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 
 import Header from './Header'
 import './Esika.css';
@@ -22,10 +22,6 @@ class Esika extends Component {
     })
   }
 
-    handleClick  = () =>{
-        console.log("clicklie");
-        
-    }
     prevSlider = () => {
         // const newIndex = ;
         this.setState({
@@ -34,75 +30,79 @@ class Esika extends Component {
           coordenadas: this.props.data.catalogue[(this.state.index - 1)].products
         })
       }
-   
-    render(){
-        console.log(this.state)
-        console.log(this.props.data.catalogue.length);
-        
+      render() {
         const style = (coordenada) => ({
-            position: "absolute",
-            cursor:"pointer",
-            left: coordenada.left,
-            top: coordenada.top,
-            width: coordenada.width,
-            height: coordenada.height,
-            tabIndex: coordenada.zindex
+          position: "absolute",
+          cursor: "pointer",
+          left: coordenada.left,
+          top: coordenada.top,
+          width: coordenada.width,
+          height: coordenada.height,
+          tabIndex: coordenada.zindex
         });
-        return(
-            <Fragment>
+    
+        return (
+          <Fragment>
             <Header />
-            <section className="contentCatalogo">
-                <div className="container slider-wrapper"> 
-                    <div className="row slider">    
-                      <img key={this.state.image} src={this.state.image}/>   
-                    </div>
-                    {this.state.coordenadas.map((obj,index)=>{
-                        return (
-                        <div key={index} style={style(obj.style)} className="contentCoordenadas">
-                            <button type="button" className="pulse-button" data-toggle="modal" data-target={`#exampleModal${index}`}>
-                </button>
-                <div className="modal fade" id={`exampleModal${index}`} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                      <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">{obj.name}</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div className="modal-body">
-                        <img src={obj.image} />
-                        <i className="fas fa-minus border p-2" onClick={this.props.reduceCount}></i>
-                        <span className="p-2">{obj.count}</span>
-                        <i className="fas fa-plus border p-2" onClick={() => this.addCount(obj.count)}></i>
-                      </div>
-                      <div className="modal-footer">
-                        <button
-                          type="button"
-                          className="btn btn-dark"
-                          onClick={() => this.props.addItem({
-                            title: obj.name,
-                            img: obj.image,
-                            webPrice: obj.webPrice,
-                            consultantPrice: obj.consultantPrice
-                          })}>Agregar a Bolsa</button>
-                      </div>
-                    </div>
-                  </div>
+            <section>
+              <div className="slider-wrapper">
+                <div className="slider">
+                  <img key={this.state.image} src={this.state.image} />
                 </div>
+                {this.state.coordenadas.map((obj, index) => {
+    
+                  return (
+                    <div key={index} style={style(obj.style)} className="contentCoordenadas">
+                      <button type="button" className="pulse-button" data-toggle="modal" data-target={`#exampleModal${index}`}>
+    
+                      </button>
+                      <div className="modal fade" id={`exampleModal${index}`} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog" role="document">
+                          <div className="modal-content">
+                            <div className="modal-header">
+                              <h5 className="modal-title" id="exampleModalLabel">{obj.name}</h5>
+                              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div className="modal-body">
+                              <img src={obj.image} className="" />
+                              {/* <i className="fas fa-minus border p-2" onClick={()=>this.props.reduceCount(obj.id)}></i>
+                            <span className="p-2">{obj.count}</span>
+                            <i className="fas fa-plus border p-2" onClick={() => this.props.addCount(obj.id)}></i> */}
+                            </div>
+                            <div className="modal-footer">
+                              <button
+                                data-dismiss="modal"
+                                aria-label="Close"
+                                type="button"
+                                className="btn btn-dark"
+                                onClick={() => this.props.addItem({
+                                  title: obj.name,
+                                  img: obj.image,
+                                  webPrice: obj.webPrice,
+                                  consultantPrice: obj.consultantPrice,
+                                  count: obj.count,
+                                  id: obj.name
+                                })}>Agregar a Bolsa</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })
+                }
+              </div >
+              <div className="buttons-wrapper">
+                <button className="prev-button" onClick={() => this.prevSlider()} disabled={this.state.index === 0}></button>
+                <button className="next-button" onClick={() => this.nextSlider()} disabled={this.state.index === this.props.data.catalogue.length}></button>
               </div>
-            )
-          })
-          }
-        </div >
-        <div className="buttons-wrapper">
-          <button className="prev-button" onClick={() => this.prevSlider()} disabled={this.state.index === 0}></button>
-          <button className="next-button" onClick={() => this.nextSlider()} disabled={this.state.index === (this.props.data.catalogue.length - 1)}></button>
-        </div>
-      </section >
-      </Fragment>
-    )
-  }
-}
-
-export default Esika
+            </section >
+          </Fragment>
+        )
+      }
+    }
+    
+    export default Esika
+    
--- src/view/Header.js
+++ src/view/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import Logo from '../assets/img/Logo.PNG';
+import Logo from './belcorp.png';
 import './Header.css'
 
 const Header = () =>{
