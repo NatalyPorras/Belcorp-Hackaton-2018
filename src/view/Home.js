aa41a9b4b385b6534c0a0d095ee885a16b012307
Nataly Porras <natalypc27@gmail.com>
2018-11-08 15:48:57 -0500

    estilos

--- src/view/Home.js
+++ src/view/Home.js
@@ -13,9 +13,9 @@ const Home = ({ data: { lbel, esika, cyzone }, itemsCount }) => {
       <Header itemsCount={itemsCount} />
       <div className="container mt-4">
         <div className="card-deck">
-          <MainCard title="ésika" image={esika.cover} link="esika" logo={esikaLogo} slogan="Tú y tu belleza lo pueden todo." />
-          <MainCard title="L'BEL" image={lbel.cover} link="lbel" logo={lbelLogo} slogan="Belleza de verdad para mujeres de verdad." />
-          <MainCard title="Cyzone" image={cyzone.cover} link="cyzone" logo={cyzoneLogo} slogan="Atrévete con actitud." />
+          <MainCard title="ésika" image={esika.cover} link="/Belcorp-Hackaton-2018/esika" logo={esikaLogo} slogan="Tú y tu belleza lo pueden todo." />
+          <MainCard title="L'BEL" image={lbel.cover} link="/Belcorp-Hackaton-2018/lbel" logo={lbelLogo} slogan="Belleza de verdad para mujeres de verdad." />
+          <MainCard title="Cyzone" image={cyzone.cover} link="/Belcorp-Hackaton-2018/cyzone" logo={cyzoneLogo} slogan="Atrévete con actitud." />
         </div>
       </div>
     </div>
